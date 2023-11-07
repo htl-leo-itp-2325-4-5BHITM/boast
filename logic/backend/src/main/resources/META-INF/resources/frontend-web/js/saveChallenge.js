9e9dfc8c0a91e59bca2b909271546f2e10ef2ac7
S.Raphael <scharrerraphael44@gmail.com>
2023-11-07 23:35:44 +0100

    Update #14, added frontend-web validation

--- logic/backend/src/main/resources/META-INF/resources/frontend-web/js/saveChallenge.js
+++ logic/backend/src/main/resources/META-INF/resources/frontend-web/js/saveChallenge.js
@@ -52,23 +52,33 @@ function showData() {
     let title = document.getElementById("title").value;
     let amountPeople = document.getElementById("amountPeople").value;
 
-    // Display the entered data
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML += `<div class="challenge">
+
+    // Validation
+    const warning = document.getElementById("warning");
+    if (creator.trim() == "" || description.trim() == "" || title.trim() == "" || amountPeople < 2) {
+        warning.innerHTML = `<div class="formError">
+                                <p> Please fill out every inputfield. </p>
+                            </div>`
+    } else {
+        warning.innerHTML = "";
+
+        // Display the entered data
+        const outputDiv = document.getElementById("output");
+        outputDiv.innerHTML += `<div class="challenge">
                                     <p><strong>Title:</strong> ${title}</p>
                                     <p><strong>Creator:</strong> ${creator}</p>
                                     <p><strong>Description:</strong> ${description}</p>
                                     <br>
                                     <h2>Contestants: </h2>`;
-    for (let i = 0; i < amountPeople; i++) {
-        outputDiv.innerHTML += `<label for="name">Name:</label>
+        for (let i = 0; i < amountPeople; i++) {
+            outputDiv.innerHTML += `<label for="name">Name:</label>
                                     <input type="text" id="name${i}" required>
                                     <label for="result">Result:</label>
                                     <input type="text" id="result${i}" required>
                                     <label for="winner">Winner: </label>
                                     <input type="radio" name="winnerBox" id="winner${i}" required>
                                     <br>`;
+        }
+        outputDiv.innerHTML += `<button type="button" onclick="uploadChallenge()">Save</button></div><br><br>`;
     }
-    outputDiv.innerHTML += `<button type="button" onclick="uploadChallenge()">Save</button></div><br><br>`;
 }
-
