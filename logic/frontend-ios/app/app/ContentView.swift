2ebb776ed14b586f8cb0297efa3fbfb12a3a1efc
JannisSideras <siderasjannis0@gmail.com>
2023-11-17 08:33:11 +0100

    initialize ios app #23

---
+++ logic/frontend-ios/app/app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Jannis Sideras on 17.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ logic/frontend-ios/app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Jannis Sideras on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
