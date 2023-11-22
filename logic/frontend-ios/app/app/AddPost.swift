aea1819577b9e4a4759270b0e7eaf2663ce89bb5
JannisSideras <siderasjannis0@gmail.com>
2023-11-22 21:17:00 +0100

    update #29

--- logic/frontend-ios/app/app/AddPost.swift
+++ logic/frontend-ios/app/app/AddPost.swift
@@ -7,14 +7,11 @@ struct AddPost: View {
     @State private var creator: String = ""
     @State private var description: String = ""
     @State private var counter: Int = 2
-    @State private var next: Bool = false
-    @State private var betCreator: String = ""
-    @State private var bet: String = ""
+    @State var details = [PostDetail]()
+    
     var body: some View {
         ScrollView {
-            
             Text("Create new Bet").font(.largeTitle)
-            
             Form {
                 TextField("Title", text: $title)
                 TextField("Creator", text: $creator)
@@ -27,12 +24,21 @@ struct AddPost: View {
             .scrollDisabled(true)
             
             ForEach(0..<counter, id: \.self){ i in
+                @State var betCreator: String = ""
+                @State var bet: String = ""
                 Form {
                     TextField("Creator \(i+1)", text: $betCreator)
                     TextField("Bet \(i+1)", text: $bet)
                 }
                 .frame(width: 400, height: 150)
             }
+            
+            Spacer(minLength: 20)
+            
+            Button("Submit Bet", action: {
+                
+            })
+            .buttonStyle(.borderedProminent)
         }
     }
 }
