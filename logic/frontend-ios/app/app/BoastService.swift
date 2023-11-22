b7c96b10bceaebfb05acdb8b976b1bb52a1c0b38
MTeofan <mihae.teo@gmail.com>
2023-11-22 21:21:50 +0100

    remove redundancy #29

--- logic/frontend-ios/app/app/BoastService.swift
+++ logic/frontend-ios/app/app/BoastService.swift
@@ -11,30 +11,3 @@ func getPostsAsList() async -> [Post] {
     }
     return posts
 }
-
-
-
-
-
-
-func getPostsAsObject() async -> [Post] {
-    let url = URL(string: "http://localhost:8080/post/getPostsAsList")!
-    var posts = [Post]()
-    if let (data, _) = try? await URLSession.shared.data(from: url ) {
-        if let loadedPosts = try? JSONDecoder().decode([Post].self, from: data) {
-            posts = loadedPosts
-        }
-    }
-    return posts
-}
-
-func getPostsAsJson() async -> [Post] {
-    let url = URL(string: "http://localhost:8080/post/getPostsAsList")!
-    var posts = [Post]()
-    if let (data, _) = try? await URLSession.shared.data(from: url ) {
-        if let loadedPosts = try? JSONDecoder().decode([Post].self, from: data) {
-            posts = loadedPosts
-        }
-    }
-    return posts
-}
--- logic/frontend-ios/app/app/Model.swift
+++ logic/frontend-ios/app/app/Model.swift
@@ -11,31 +11,6 @@ struct Model {
         let id = UUID()
         let bet, creator: String
     }
-    struct DeleteThis: Codable {
-        let id = UUID()
-        let bet: String
-        let creator2: String
-        let bet2: String
-        let creator3: String
-        let bet3: String
-        let creator4: String
-        let bet4: String
-        let creator5: String
-        let bet5: String
-        let creator6: String
-        let bet6: String
-        let creator7: String
-        let bet7: String
-        let creator8: String
-        let bet8: String
-        let creator9: String
-        let bet9: String
-        let creator10: String
-        let bet81: String
-        let creator11: String
-        let bet22: String
-        let creator33: String
-    }
 }
 
 typealias Post = Model.Post
