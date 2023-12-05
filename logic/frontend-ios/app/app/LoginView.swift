36b7c4d3c8fe36a5d96ff71cd6898b28b3b3dc3c
JannisSideras <siderasjannis0@gmail.com>
2023-12-05 17:15:22 +0100

    update #38

---
+++ logic/frontend-ios/app/app/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  app
+//
+//  Created by Jannis Sideras on 05.12.23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
