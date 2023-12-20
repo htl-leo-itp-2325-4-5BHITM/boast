ec43d2d844d69a92a4d02790ce65e0b90cdc7717
JannisSideras <siderasjannis0@gmail.com>
2023-12-20 14:11:17 +0100

    update #44 added join in option in view. route not implemented yet TODO

--- logic/frontend-ios/app/app/View/PostView.swift
+++ logic/frontend-ios/app/app/View/PostView.swift
@@ -7,6 +7,7 @@ struct PostView: View {
     @State var post: PollPostViewModel?
     @State var dict = [Int:String]()
     @State var postStatus:Status = .CLOSED
+    @State var answerOption = ""
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
@@ -41,16 +42,39 @@ struct PostView: View {
             }
             .background(Color.red.opacity(0.1))
             
-            VStack(alignment: .leading) {
-                ForEach(post?.postDetails ?? [Poll_PostDetailModel](), id: \.postDetailsId) { detail in
-                    HStack {
-                        Text(detail.createdOn ?? "")
-                        Text("\(detail.creatorName ?? "") :")
-                        Text("\(dict[detail.poll_answerId ?? 0] ?? "")")
+            HStack {
+                VStack(alignment: .leading) {
+                    ForEach(post?.postDetails ?? [Poll_PostDetailModel](), id: \.postDetailsId) { detail in
+                        HStack {
+                            Text(detail.createdOn ?? "")
+                            Text("\(detail.creatorName ?? "") :")
+                            Text("\(dict[detail.poll_answerId ?? 0] ?? "")")
+                        }
+                    }
+                    
+                }
+                .background(Color.yellow.opacity(0.1))
+                
+                VStack(alignment: .trailing) {
+                    if(post?.creatorId != UserDefaults.standard.integer(forKey: "userId")){
+                        Picker("answers", selection: $answerOption) {
+                            Text("No Answer")
+                                .tag(-1)
+                            ForEach(Array(dict.values), id: \.self) { val in
+                                Text("\(val)")
+                                    .tag(val)
+                            }
+                        }
+                        .onChange(of: postStatus) {
+                            Task {
+                                do {
+                                    // TODO
+                                }
+                            }
+                        }
                     }
                 }
             }
-            .background(Color.yellow.opacity(0.1))
         }.task {
             post = await PollPostViewModel(model: loadPost(postId: postId) as! Poll_PostModel)
             for answer in post?.typeInfo.pollAnswers ?? [Poll_PostAnswerModel]() {
