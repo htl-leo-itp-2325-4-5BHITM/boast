be9c852da734da90a1a9e05925102f1870b5dce0
MTeofan <mihae.teo@gmail.com>
2023-12-21 19:20:41 +0100

    update import sql

--- logic/backend/src/main/resources/import.sql
+++ logic/backend/src/main/resources/import.sql
@@ -13,59 +13,75 @@ VALUES (current_timestamp, 103, 'Teo', 'teofan.mihaescu@gmail.com');
 
 --POST
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 103, 100, 'Swift Test',
-        'Do you think that we have to retake the Test in MC?', 'POLL', 'OPEN');
-
-insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 100, 101, 'Buzz Cut',
-        'Should I buzz my hair?', 'POLL', 'OPEN');
-
-insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 101, 102, 'Athletic',
-        'Am I the most athletic guy at school?',
+values (current_timestamp, 103, 100,
+        'Swift Test',
+        'We will have to retake the Test in MC',
         'POLL', 'OPEN');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 100, 103, 'New Car',
-        'Should I buy another Tesla?',
+values (current_timestamp, 100, 101,
+        'School',
+        'Teo will not be able to have an autism-free day this week',
+        'POLL', 'OPEN');
+
+insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
+values (current_timestamp, 101, 102,
+        'UFC 293',
+        'Strickland wont win against Adesanya',
+        'POLL', 'OPEN');
+
+insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
+values (current_timestamp, 100, 103,
+        'Attendance',
+        'Raphi will be 5min late today',
         'POLL', 'CLOSED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 102, 104, 'Smoking',
-        'Should I quit smoking and start taking crack?',
+values (current_timestamp, 102, 104,
+        'Smoking',
+        'Felix will start smoking by the end of February',
         'POLL', 'CLOSED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 103, 105, 'Eating',
-        'Should the bulk continue?', 'POLL',
-        'CANCELED');
-
-insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 103, 106, 'Class',
-        'Do you think that everybody in our class will make it to 5BHITM?',
+values (current_timestamp, 101, 105,
+        'Eating',
+        'After next years Summer break Teo will be overweight',
         'POLL', 'CANCELED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 101, 107, 'Adult',
-        'Do you think that I should act more maturely?',
+values (current_timestamp, 103, 106,
+        'Class',
+        'Everybody in our class will make it to 5BHITM',
+        'POLL', 'CANCELED');
+
+insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
+values (current_timestamp, 102, 107,
+        '1. Sem ending',
+        'Teo will have a grade average of 3.5 by the end of the first Semester',
         'POLL', 'OPEN');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 102, 108, 'Hair',
-        'Should I change my Hairstyle?',
+values (current_timestamp, 102, 108,
+        'Hair',
+        'Raphi will start balding at 21.',
         'POLL', 'CLOSED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 103, 109, 'Training',
-        'Should I start calisthenics?', 'POLL', 'CLOSED');
+values (current_timestamp, 101, 109,
+        'Football',
+        'Italy will win the EM.',
+        'POLL', 'CLOSED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 100, 110, 'Sigma',
-        'Do you think that I should stop helping Classmates?', 'POLL', 'CLOSED');
+values (current_timestamp, 100, 110,
+        'Ronaldo retirement',
+        'Ronaldo will retire in 5 years',
+        'POLL', 'CLOSED');
 
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
-values (current_timestamp, 102, 111, 'Aggression',
-        'I want to beat Julian Jaklitsch to a pulp. Should I do that?',
+values (current_timestamp, 102, 111,
+        'School',
+        'The 2nd Semester will be harder than the first',
         'POLL', 'CLOSED');
 
 --POSTDETAIL
@@ -183,10 +199,10 @@ values (111);
 
 --POLL_POSTANSWER
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (100, 1000, 'yes');
+VALUES (100, 1000, 'never');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (100, 1001, 'no');
+VALUES (100, 1001, 'it is likely');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (101, 1002, 'definitely');
@@ -195,31 +211,28 @@ insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (101, 1003, 'definitely not');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (102, 1004, 'sure buddy');
+VALUES (102, 1004, 'very possible');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (102, 1005, 'LMAO');
+VALUES (102, 1005, 'sure buddy');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (103, 1006, 'ofc');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (103, 1007, 'nah, you broke');
+VALUES (103, 1007, 'nah');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (103, 1008, 'start gambling instead');
+VALUES (104, 1009, 'never');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (104, 1009, 'Yes');
+VALUES (104, 1010, 'yeah, maybe');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (104, 1010, 'YES');
+VALUES (105, 1011, 'yes');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (105, 1011, 'start cutting');
-
-insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (105, 1012, 'yes, you are skinny');
+VALUES (105, 1012, 'no');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (106, 1013, 'No');
@@ -228,34 +241,37 @@ insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (106, 1014, 'Yes');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (107, 1015, 'Do it already');
+VALUES (107, 1015, 'over');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (107, 1016, 'stay a child');
+VALUES (107, 1016, 'under');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (108, 1017, 'Pls do not');
+VALUES (108, 1017, 'definitely');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (108, 1018, 'about time');
+VALUES (108, 1018, 'hairline will stay');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (109, 1019, 'NO. You are fat');
+VALUES (109, 1019, 'Italy for the win');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (109, 1020, 'Give it a shot');
+VALUES (109, 1020, 'Turkey will win');
+
+insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
+VALUES (109, 1020, 'France will win');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (110, 1021, 'Definitely');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (110, 1022, 'Skibidi Toilet');
+VALUES (110, 1022, 'after 5 years');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
 VALUES (111, 1023, 'yeah sure');
 
 insert into POLL_POSTANSWER (POLLPOST_POSTID, POLL_ANSWERID, TITLE)
-VALUES (111, 1024, 'DO NOT HURT MY POOKIE');
+VALUES (111, 1024, 'can not be');
 
 --POLL_POSTDETAIL
 insert into POLL_POSTDETAIL (POLLANSWER_POLL_ANSWERID, POSTDETAILSID, POSTID)
@@ -332,160 +348,3 @@ VALUES (1023, 1023, 111);
 
 insert into POLL_POSTDETAIL (POLLANSWER_POLL_ANSWERID, POSTDETAILSID, POSTID)
 VALUES (1023, 1024, 111);
-
---IGNORE FOR NOW
-/*insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-VALUES (1000, 'Teo', 'Raphi', 'Workout Challenge',
-        'Try to do 10 Sit-Ups in the shortest possible time and submit your time in seconds');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1001, 'Felix', 'Teo', 'Blindfolded Food Eating Challenge',
-        'Blindfold a friend and have them eat random foods. The one who guesses most of them wins');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1002, 'Raphi', 'Jannis', 'Ice Bath Challenge',
-        'Fill your bathtub up with cold water and then add 3kg of ice, the person who stays in there the longest wins');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1003, 'Felix', 'Raphi', 'Balloon-In-Air Challenge',
-        'Each person gets a balloon filled up with Radon [Rn 86]. Try keeping the balloon airborne by blowing against it.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1004, 'Jannis', 'Felix', 'Building A Cheese Cracker House',
-        'Everybody knows how to build a tower out of card, now do it with Cheez its. The Person who accomplishes this the fastest wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1005, 'Teo', 'Felix', 'Mimicry Challenge',
-        'Impersonate someone we know. After 30 rounds, the person with the most correct guesses wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1006, 'Teo', 'Raphi', 'Stuffing A Pancake Challenge',
-        'Prepare huge pancakes for this food challenge, and whoever finishes their pancake first wins. Hot-Sauce must be poured on to the pancake.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1007, 'Raphi', 'Jannis', 'Brain Freezer Challenge',
-        'Stand in ice cold water and answer trivia questions. With every wrong questions more ice gets added. Answer most questions correctly and wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1008, 'Jannis', 'Teo', 'Talk To A Stranger',
-        'Your friends need to go out and start a conversation with a random stranger. The one to keep the conversation going the longest wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1009, 'Teo', 'Raphi', 'Drinking Challenge',
-        'Drink a shot accompanied by 3 push ups. The one who drank the most shots wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1010, 'Felix', 'Teo', 'Jump Rope Challenge',
-        'The person who manages to continue skipping the longest wins.');
-
-insert into POST (POSTID, CREATOR, WINNER, TITLE, DEFINITION)
-values (1011, 'Jannis', 'Jannis', 'Silent Library',
-        'Be an asshole and make noise in a library for 5min. The one who gets shushed the least amount of times wins.');
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1000, 'Raphi', '16s', 1000);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1001, 'Felix', '19s', 1000);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1002, 'Jannis', '19s', 1000);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1003, 'Teo', '17s', 1000);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1004, 'Raphi', '5 guesses', 1001);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1005, 'Felix', '6 guesses', 1001);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1006, 'Jannis', '9 guesses', 1001);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1007, 'Teo', '15 guesses', 1001);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1008, 'Raphi', '3min 40s', 1002);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1009, 'Felix', '32s', 1002);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1010, 'Jannis', '10min', 1002);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1011, 'Teo', '5min 33s', 1002);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1012, 'Raphi', '21s', 1003);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1013, 'Felix', '19s', 1003);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1014, 'Jannis', '5s', 1003);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1015, 'Teo', '12s', 1003);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1016, 'Raphi', '52s', 1004);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1017, 'Felix', '27s', 1004);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1018, 'Jannis', '1min', 1004);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1019, 'Teo', '2min 13s', 1004);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1020, 'Raphi', '6 guesses', 1005);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1021, 'Felix', '13 guesses', 1005);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1022, 'Jannis', '7 guesses', 1005);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1023, 'Teo', '4 guesses', 1005);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1024, 'Raphi', '8s', 1006);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1025, 'Felix', '13s', 1006);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1026, 'Jannis', '12s', 1006);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1027, 'Teo', '10s', 1006);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1028, 'Raphi', '11/15 correct', 1007);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1029, 'Felix', '12/15 correct', 1007);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1030, 'Jannis', '13/15 correct', 1007);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1031, 'Teo', '12/15 correct', 1007);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1032, 'Raphi', '1min 5s', 1008);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1033, 'Felix', '2min 41s', 1008);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1034, 'Jannis', '3min 19s', 1008);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1035, 'Teo', '7min 4s', 1008);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1036, 'Raphi', '13 shots', 1009);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1037, 'Felix', '4 shots', 1009);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1038, 'Jannis', '8 shots', 1009);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1039, 'Teo', 'puked...', 1009);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1040, 'Raphi', '1min 39s', 1010);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1041, 'Felix', '1min 12s', 1010);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1042, 'Jannis', '44s', 1010);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1043, 'Teo', '1min 54s', 1010);
-
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1044, 'Raphi', '7 shushes', 1011);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1045, 'Felix', '5 shushes', 1011);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1046, 'Jannis', '0 shushes', 1011);
-insert into POSTDETAIL (POSTDETAILSID, CREATOR, BET, POSTID)
-VALUES (1047, 'Teo', '14 shushes', 1011);*/
