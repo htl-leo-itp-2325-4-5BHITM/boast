fa39bb0a5c865ee7df8ab7bba3899c87d77c99fb
MTeofan <mihae.teo@gmail.com>
2024-01-17 22:35:08 +0100

    test users added

--- logic/backend/src/main/resources/import.sql
+++ logic/backend/src/main/resources/import.sql
@@ -11,6 +11,70 @@ VALUES (current_timestamp, 102, 'Jannis', 'jannis.sideras@gmail.com');
 insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
 VALUES (current_timestamp, 103, 'Teo', 'teofan.mihaescu@gmail.com');
 
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 104, 'Andrei', 'andrei.brehar@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 105, 'Lana', 'lana.sekerija@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 106, 'Yasin', 'yasin.erkol@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 107, 'Sebi', 'sebastian.feichtenschlager@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 108, 'Julian', 'julian.jaklitsch@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 109, 'elYanuki', 'yanik.kendler@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 110, 'Jana', 'jana.krenn@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 111, 'Michi', 'michael.leisch@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 112, 'Tommy', 'tommy.neumaier@icloud.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 113, 'Malte', 'malte.ostermann@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 114, 'Ana', 'ana.paripovic@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 115, 'Sara', 'sara.radic@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 116, 'Oli', 'oliver.schatzberger@icloud.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 117, 'Nati', 'natalie.schmitzberger@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 118, 'Romana', 'romana.schned@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 119, 'Leonie', 'leonie.schwarz@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 120, 'Tuana', 'tuana.sevic@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 120, 'Leon', 'leon.steinhuber@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 120, 'Heiko', 'heiko.vichtbauer@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 120, 'Janine', 'janine.wenninger@gmail.com');
+
+insert into BOASTUSER (CREATEDON, USERID, USERNAME, EMAIL)
+VALUES (current_timestamp, 120, 'Mario', 'mario.meister@gmail.com');
+
+
 --POST
 insert into POST (CREATEDON, CREATOR_USERID, POSTID, TITLE, DEFINITION, POSTTYPE, STATUS)
 values (current_timestamp, 103, 100,
